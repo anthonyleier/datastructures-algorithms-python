8fcc5a80749ce502daac60eb86399fc30ea9c548
Anthony Cruz <Anthonyleierlw@gmail.com>
2023-06-13 14:07:12 -0300

    Binary tree com pós ordem

--- binary_tree.py
+++ binary_tree.py
@@ -16,24 +16,38 @@ class BinaryTree:
         else:
             self.root = None
 
-    # Percurso em ordem simétrica
-    def simetric_traversal(self, node=None):
+    # Percurso em ordem simétrica (InOrder)
+    def inorder_traversal(self, node=None):
         if not node:
             node = self.root
 
         if node.left:
-            print('(', end='')
-            self.simetric_traversal(node.left)
+            # print('(', end='')
+            self.inorder_traversal(node.left)
 
         print(node, end='')
 
         if node.right:
-            self.simetric_traversal(node.right)
-            print(')', end='')
+            self.inorder_traversal(node.right)
+            # print(')', end='')
+
+    # Percurso em Pós-Ordem (PostOrder)
+    def postorder_traversal(self, node=None):
+        if not node:
+            node = self.root
+
+        if node.left:
+            self.postorder_traversal(node.left)
+
+        if node.right:
+            self.postorder_traversal(node.right)
+
+        print(node, end='')
 
 
 if __name__ == "__main__":
-    binary_tree = BinaryTree()
+    # InOrder
+    inorder_tree = BinaryTree()
     n1 = Node('a')
     n2 = Node('+')
     n3 = Node('*')
@@ -46,15 +60,38 @@ if __name__ == "__main__":
 
     n6.left = n7
     n6.right = n8
-
     n5.left = n6
     n5.right = n9
-
     n3.left = n4
     n3.right = n5
-
     n2.left = n1
     n2.right = n3
+    inorder_tree.root = n2
+    inorder_tree.inorder_traversal()
 
-    binary_tree.root = n2
-    binary_tree.simetric_traversal()
+    print()
+    # PostOrder
+    postorder_tree = BinaryTree()
+    n1 = Node('A')
+    n2 = Node('B')
+    n3 = Node('C')
+    n4 = Node('D')
+    n5 = Node('E')
+    n6 = Node('F')
+    n7 = Node('G')
+    n8 = Node('H')
+    n9 = Node('I')
+    n0 = Node('J')
+
+    n0.left = n6
+    n0.right = n9
+    n6.left = n1
+    n6.right = n5
+    n5.left = n2
+    n5.right = n4
+    n4.right = n3
+    n9.left = n8
+    n8.right = n7
+
+    postorder_tree.root = n0
+    postorder_tree.postorder_traversal()
