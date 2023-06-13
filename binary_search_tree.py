9eb89b7e7d5824dbe9e7f48cd4b0d060668d065d
Anthony Cruz <Anthonyleierlw@gmail.com>
2023-06-13 16:03:06 -0300

    Encontrar o maior e o menor valor

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -10,14 +10,14 @@ class BinarySearchTree(BinaryTree):
             parent = pivot
             if value < pivot.value:
                 pivot = pivot.left
-            elif value > pivot.value:
+            else:  # value > pivot.value
                 pivot = pivot.right
 
         if not parent:
             self.root = Node(value)
         elif value < parent.value:
             parent.left = Node(value)
-        elif value > parent.value:
+        else:  # value > parent.value
             parent.right = Node(value)
 
     def search(self, value, node=ROOT):
@@ -33,6 +33,24 @@ class BinarySearchTree(BinaryTree):
         if value > node.value:
             return self.search(value, node.right)
 
+    def get_lowest_value(self, node=ROOT):
+        if node == ROOT:
+            node = self.root
+
+        if node.left:
+            return self.get_lowest_value(node.left)
+        else:
+            return node
+
+    def get_highest_value(self, node=ROOT):
+        if node == ROOT:
+            node = self.root
+
+        if node.right:
+            return self.get_highest_value(node.right)
+        else:
+            return node
+
 
 if __name__ == "__main__":
     binary_search_tree = BinarySearchTree()
@@ -41,6 +59,8 @@ if __name__ == "__main__":
     binary_search_tree.insert(12)
     binary_search_tree.insert(89)
     binary_search_tree.insert(27)
+    binary_search_tree.insert(22)
+    binary_search_tree.insert(22)
 
     print('Inorder Traversal')
     binary_search_tree.inorder_traversal()
@@ -59,3 +79,6 @@ if __name__ == "__main__":
 
     print('Buscando por 33:', binary_search_tree.search(33).root)
     print('Buscando por 30:', binary_search_tree.search(30).root)
+
+    print('O menor valor é', binary_search_tree.get_lowest_value())
+    print('O maior valor é', binary_search_tree.get_highest_value())
